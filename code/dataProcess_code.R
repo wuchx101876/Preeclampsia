0d8845b1061bbd61e77bb2e5af8225fcfbb36514
wuchx <chenxuwu1018@163.com>
2025-07-29 21:28:26 +0800

    update

--- code/dataProcess_code.R
+++ code/dataProcess_code.R
@@ -47,6 +47,22 @@ saveRDS(use_clin1,"./use_data/GSE25906_clin.rds")
 
 
 
-
+## GSE48424
+library(AnnoProbe)
+library(GEOquery) 
+geo_data <- getGEO(GEO = "GSE48424", destdir = './raw_data/GSE48424/', getGPL = F)
+geo_data0 <- geo_data[[1]]
+exp <- as.data.frame(Biobase::exprs(geo_data0)) 
+ids <- fread("./raw_data/GSE48424/GPL6480-9577.txt",data.table = F)
+ids1 <- ids[,c(1,7)]
+colnames(ids1) <- c("probe_id","symbol")
+exp$probe_id <- rownames(exp)
+exp1 <- dplyr::inner_join(ids1,exp)
+library(limma)
+exp2 <- exp1[,-1]
+exp3 <- as.data.frame(limma::avereps(exp2[,-1],ID = exp2$symbol))
+saveRDS(exp3,"./raw_data/GSE48424/GSE48424_exp.rds")
+cli <- Biobase::pData(geo_data0)
+saveRDS(cli,"./raw_data/GSE48424/GSE48424_cli.rds")
 
 
